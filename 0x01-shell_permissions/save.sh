99c473dbd1baa2a14f11515f2d0b6b6d4a141314
khalidelaasry <khalidelaasry22@gmail.com>
2023-08-31 23:26:52 +0200

    correct save.sh

--- 0x01-shell_permissions/save.sh
+++ 0x01-shell_permissions/save.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Add the changes to the index
+echo "input your message"
+read var
 git add .
 
 # Commit the changes
-git commit -m "Automated commit: Save script"
+git commit -m "${var}"
+git push
 
